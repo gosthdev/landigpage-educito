1d59a7931d5de3ba6336b02bb1ff9442de31e180
gosth <augustusdm8@gmail.com>
2026-06-21 19:51:17 -0500

    configurar exportación estática y permitir S3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com"
-      }
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com", 
+      },
     ]
   }
 };
